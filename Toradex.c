ccea283c451a174c48b41f2f9a52a1363d047813
VictorMoreiraSantos <88212397+VictorMoreiraSantos@users.noreply.github.com>
2021-08-02 17:35:27 -0300

    Update and rename Toradex-Mbed to Toradex.c

---
+++ Toradex.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+
