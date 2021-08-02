8199158b60101f0614b92066de99f74a83009631
VictorMoreiraSantos <88212397+VictorMoreiraSantos@users.noreply.github.com>
2021-08-02 18:02:58 -0300

    Update Toradex.c

--- Toradex.c
+++ Toradex.c
@@ -12,4 +12,31 @@
 #include <linux/can.h>
 #include <linux/can/raw.h>
 
+void main(){
+  int dir=0;
+  char c;
+  while(1){
+    dir = leitura();
+  
+    // Enviar dir via CAN
+    delay(3000)
+  }
+}
 
+int leitura(void){
+  scanf("%c", $c);
+  if(c == "w"){
+  dir = 2; //frente
+  }
+  else if(c == "a"){
+  dir = 3; //esquerda
+  }
+  else if(c == "d"){
+  dir = 4; //direita
+  }
+  else if(c == "s"){
+  dir = 5; //tras
+  }
+return dir;
+}
+  
